fe34785f47165ab371cdbd9f212a6f8ec9406fda
zhi bin zhuang <zhi bin zhuang>
2016-09-11 22:59:23 +0800

    The radio bug still here

--- src/main/java/com/karl/fx/controller/MainDeskController.java
+++ src/main/java/com/karl/fx/controller/MainDeskController.java
@@ -3,18 +3,18 @@ package com.karl.fx.controller;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Callback;
 
@@ -61,38 +61,9 @@ public class MainDeskController extends FxmlController {
 
     @FXML
     private void flushGroup(ActionEvent event) {
-        // webWechat.wxInit();
-        webWechat.getContact();
+        webWechat.wxInit();
+        // webWechat.getContact();
         groupChoiseTab.setItems(generateDataInMap());
-    }
-
-    private class RadioButtonTableCell<S, T> extends TableCell<S, T> {
-        private final RadioButton radio;
-        private ObservableValue<T> ov;
-
-        public RadioButtonTableCell() {
-            this.radio = new RadioButton();
-            setAlignment(Pos.CENTER);
-            setGraphic(radio);
-        }
-
-        @Override
-        protected void updateItem(T item, boolean empty) {
-            super.updateItem(item, empty);
-            if (empty) {
-                setText(null);
-                setGraphic(null);
-            } else {
-                setGraphic(radio);
-                if (ov instanceof SimpleBooleanProperty) {
-                    radio.selectedProperty().unbindBidirectional((SimpleBooleanProperty) ov);
-                }
-                ov = getTableColumn().getCellObservableValue(getIndex());
-                if (ov instanceof SimpleBooleanProperty) {
-                    radio.selectedProperty().bindBidirectional((SimpleBooleanProperty) ov);
-                }
-            }
-        }
     }
 
     @SuppressWarnings("unchecked")
@@ -100,28 +71,14 @@ public class MainDeskController extends FxmlController {
         groupChoiseTab.setEditable(true);
         groupChoiseTab.setItems(generateDataInMap());
         TableColumn<ChatGroup, Boolean> selectColumn = new TableColumn<>("选择");
-        selectColumn.setCellValueFactory(new PropertyValueFactory<ChatGroup, Boolean>(
-                ChatGroup.selectorColumnKey));
-        selectColumn
-                .setCellFactory(new Callback<TableColumn<ChatGroup, Boolean>, TableCell<ChatGroup, Boolean>>() {
-                    @Override
-                    public TableCell<ChatGroup, Boolean> call(TableColumn<ChatGroup, Boolean> param) {
-                        final RadioButtonTableCell<ChatGroup, Boolean> cell = new RadioButtonTableCell<>();
-                        final RadioButton radio = (RadioButton) cell.getGraphic();
-                        radio.setOnAction(new EventHandler<ActionEvent>() {
-                            @Override
-                            public void handle(ActionEvent event) {
-                                for (ChatGroup group : groupChoiseTab.getItems()) {
-                                    group.setSelector(false);
-                                }
-                                ChatGroup group = groupList.get(cell.getIndex());
-                                group.setSelector(true);
-                                runtimeDomain.setCurrentGroupId(group.getGroupId());
-                            }
-                        });
-                        return cell;
-                    }
-                });
+
+        Callback<TableColumn<ChatGroup, Boolean>, TableCell<ChatGroup, Boolean>> cellFactory = new Callback<TableColumn<ChatGroup, Boolean>, TableCell<ChatGroup, Boolean>>() {
+            @Override
+            public TableCell<ChatGroup, Boolean> call(TableColumn<ChatGroup, Boolean> p) {
+                return new RadioButtonCell();
+            }
+        };
+        selectColumn.setCellFactory(cellFactory);
 
         TableColumn<ChatGroup, String> qunNameColumn = new TableColumn<>("群名称");
         TableColumn<ChatGroup, Integer> qunSizeColumn = new TableColumn<>("群人数");
@@ -169,6 +126,57 @@ public class MainDeskController extends FxmlController {
 
         public void setSelector(Boolean selector) {
             this.selector = new SimpleBooleanProperty(selector);
+        }
+    }
+
+    class RadioButtonCell extends TableCell<ChatGroup, Boolean> {
+
+        final ToggleGroup group = new ToggleGroup();
+
+        private RadioButton radio;
+
+        public RadioButtonCell() {
+            createRadioButton();
+        }
+
+        private void createRadioButton() {
+            radio = new RadioButton();
+            radio.focusedProperty().addListener(new ChangeListener<Boolean>() {
+                @Override
+                public void changed(ObservableValue<? extends Boolean> arg0, Boolean arg1,
+                        Boolean arg2) {
+                    if (!arg2) {
+                        commitEdit(radio.isSelected());
+                    }
+                }
+            });
+            radio.setToggleGroup(group);
+        }
+
+        @Override
+        public void commitEdit(Boolean t) {
+            super.commitEdit(t);
+            final ObservableList<ChatGroup> items = getTableView().getItems();
+            for (int i = 0; i < items.size(); i++) {
+                ChatGroup chatGroup = items.get(i);
+                if (i == getIndex()) {
+                    chatGroup.setSelector(t);
+                    runtimeDomain.setCurrentGroupId(chatGroup.getGroupId());
+                } else {
+                    chatGroup.setSelector(Boolean.FALSE);
+                }
+            }
+        }
+
+        @Override
+        public void updateItem(Boolean item, boolean empty) {
+            super.updateItem(item, empty);
+            final ObservableList<ChatGroup> items = getTableView().getItems();
+            if (items != null) {
+                if (getIndex() < items.size()) {
+                    setGraphic(radio);
+                }
+            }
         }
     }
 }
